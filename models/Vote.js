e33a64b1db6edf962ff117d2d1ba119c1112fe8e
Sir Ubu <thesirubu@gmail.com>
2021-06-06 20:21:10 -0600

    Added vote table model and association to other models.

---
+++ models/Vote.js
@@ -0,0 +1,42 @@
+// require dependencies
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create vote table model 
+class Vote extends Model {};
+
+Vote.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'vote'
+    }
+);
+
+// export vote table model
+module.exports = Vote;
