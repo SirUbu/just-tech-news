47838e47731b3c24ee30a51f0a5131e863078099
Sir Ubu <thesirubu@gmail.com>
2021-06-07 16:10:53 -0600

    Added homepage with Handlebars.js.

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 // require dependencies
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 // set url path for apis
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 // set url path for any other path
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // require dependencies
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+// require Handlebars.js
+const exphbs = require('express-handlebars');
 
 // create express server
 const app = express();
@@ -10,9 +13,16 @@ const PORT = process.env.PORT || 3001;
 // add middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+// set up Handlebars.js as app template engine
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
